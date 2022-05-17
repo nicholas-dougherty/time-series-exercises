ba64739d4d993028a116ce1a4524eca3e58ddd88
Nicholas Dougherty <nicholas.j.dougherty@gmail.com>
2022-05-17 11:16:12 -0500

    beepboop

--- acquire.py
+++ acquire.py
@@ -1,20 +1,28 @@
+#  ________  ________  ________  ___  ___  ___  ________  _______      
+# |\   __  \|\   ____\|\   __  \|\  \|\  \|\  \|\   __  \|\  ___ \     
+# \ \  \|\  \ \  \___|\ \  \|\  \ \  \\\  \ \  \ \  \|\  \ \   __/|    
+#  \ \   __  \ \  \    \ \  \\\  \ \  \\\  \ \  \ \   _  _\ \  \_|/__  
+#   \ \  \ \  \ \  \____\ \  \\\  \ \  \\\  \ \  \ \  \\  \\ \  \_|\ \ 
+#    \ \__\ \__\ \_______\ \_____  \ \_______\ \__\ \__\\ _\\ \_______\
+#     \|__|\|__|\|_______|\|___| \__\|_______|\|__|\|__|\|__|\|_______|
+#                               \|__|                                                                                                                                                                          
 '''
 Module to encapsulate data acquisition.
 Primary functions to use are
 - get_store_item_demand_data()
 - get_opsd_data()
 
-If you want to get the individual datasets from the store item demand data you
-can do so with
+If you want to get the individual datasets from the store
+item demand data you can do so with
 - get_items_data
 - get_stores_dat
 - get_sales_data
 '''
-
+#----------------------------------------------------------------------------|
 import os
 import requests
 import pandas as pd
-
+#----------------------------------------------------------------------------|
 def get_store_data_from_api():
     domain = 'https://python.zgulde.net'
     endpoint = '/api/v1/stores'
@@ -25,7 +33,7 @@ def get_store_data_from_api():
     stores = pd.DataFrame(data['payload']['stores'])
     stores = pd.DataFrame(stores)
     return stores
-
+#----------------------------------------------------------------------------|
 def get_items_data_from_api():
     domain = 'https://api.data.codeup.com'
     endpoint = '/api/v1/items'
@@ -34,14 +42,15 @@ def get_items_data_from_api():
         url = domain + endpoint
         response = requests.get(url)
         data = response.json()
-        print(f'\rGetting page {data["payload"]["page"]} of {data["payload"]["max_page"]}: {url}', end='')
+        print(f'\rGetting page {data["payload"]["page"]} of
+              {data["payload"]["max_page"]}: {url}', end='')
         items.extend(data['payload']['items'])
         endpoint = data['payload']['next_page']
         if endpoint is None:
             break
     items = pd.DataFrame(items)
     return items
-
+#----------------------------------------------------------------------------|
 def get_sales_data_from_api():
     base_url = 'https://api.data.codeup.com/api/v1/sales?page='
     sales = []
@@ -60,28 +69,28 @@ def get_sales_data_from_api():
         sales.extend(data['payload']['sales'])
     sales = pd.DataFrame(sales)
     return sales
-
+#----------------------------------------------------------------------------|
 def get_stores_data():
     if os.path.exists('stores_df.csv'):
         return pd.read_csv('stores_df.csv')
     df = get_store_data_from_api()
     df.to_csv('stores_df.csv', index=False)
     return df
-
+#----------------------------------------------------------------------------|
 def get_items_data():
     if os.path.exists('items_df.csv'):
         return pd.read_csv('items_df.csv')
     df = get_items_data_from_api()
     df.to_csv('items_df.csv', index=False)
     return df
-
+#----------------------------------------------------------------------------|
 def get_sales_data():
     if os.path.exists('sales_df.csv'):
         return pd.read_csv('sales_df.csv')
     df = get_sales_data_from_api()
     df.to_csv('sales_df.csv', index=False)
     return df
-
+#----------------------------------------------------------------------------|
 def get_store_item_demand_data(use_cache=True):
     if os.path.exists('sales_stores_items.csv') and use_cache:
         print('Using cached CSV')
@@ -101,10 +110,11 @@ def get_store_item_demand_data(use_cache=True):
     df.to_csv('sales_stores_items.csv')
 
     return df
-
+#----------------------------------------------------------------------------|
 def get_opsd_data():
     if os.path.exists('opsd.csv'):
         return pd.read_csv('opsd.csv')
     df = pd.read_csv('https://raw.githubusercontent.com/jenfly/opsd/master/opsd_germany_daily.csv')
     df.to_csv('opsd.csv', index=False)
     return df
+#----------------------------------------------------------------------------|
